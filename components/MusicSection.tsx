b1915f9fb865378d775e35e3d33b3647d591999e
mauroibarra133 <mauroibarra133@gmail.com>
2026-07-06 22:23:48 -0300

    Add collaborative button to Spotify music section

--- components/MusicSection.tsx
+++ components/MusicSection.tsx
@@ -56,6 +56,19 @@ export const MusicSection: React.FC = () => {
             loading="lazy"
           ></iframe>
         </div>
+
+        {/* Botón Colaborativo para agregar canciones */}
+        <a
+          href="https://open.spotify.com/playlist/2t9pyEpO0EvJB5bkjtCHQ7?si=AcH1litLROGmRiULRNNrnw&pi=awcVY9CzSKGhY&pt_success=1&nd=1&dlsi=b8cfbc1d662e4cb3"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.spotifyButton}
+        >
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm4.586 14.424c-.18.295-.565.387-.86.207-2.377-1.454-5.37-1.783-8.894-1.007-.336.08-.668-.135-.745-.47-.078-.337.136-.669.471-.746 3.847-.85 7.15-.477 9.822 1.156.295.18.387.563.207.86zm1.224-2.723c-.226.367-.707.487-1.074.26-2.717-1.67-6.864-2.15-10.07-1.177-.412.125-.845-.105-.97-.517-.124-.412.106-.846.518-.971 3.66-1.11 8.225-.572 11.33 1.34.367.227.488.708.266 1.065zm.106-2.828C14.562 8.87 9.07 8.687 5.89 9.65c-.53.16-1.09-.14-1.25-.67-.16-.53.14-1.09.67-1.25 3.66-1.11 9.72-.9 13.59 1.4.48.28.64.9.36 1.38-.28.48-.9.64-1.38.36z"/>
+          </svg>
+          Agregar música a la Playlist
+        </a>
       </div>
 
       {/* MODAL DE AGRADECIMIENTO */}
